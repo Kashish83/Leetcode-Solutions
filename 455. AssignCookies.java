b624a4d3f04143d33cf943a3d7c29c205d569a4a
kashish wadhwa <kashish1183.becse24@chitkara.edu.in>
2025-07-03 10:36:04 +0530

    Added 455. AssignCookies.java

---
+++ 455. AssignCookies.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int child=0;
+        int cookie=0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        while(child<g.length && cookie<s.length){
+            if(s[cookie]>=g[child]){
+                child++;
+            }
+            cookie++;
+        }
+         return child;
+    }
+   
+}
