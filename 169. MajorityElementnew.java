e1478c24f70bf28399af6aa96f3de5055bf8a695
kashish wadhwa <kashish1183.becse24@chitkara.edu.in>
2025-07-09 22:44:00 +0530

    Added 169. MajorityElementsecondsol.java

---
+++ 169. MajorityElementnew.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+      
+        for(int i=0; i<nums.length; i++){
+            // if(map.ContainsKey(nums[i])){
+            //     map.put(nums[i],map.get(nums[i])+1);
+            // }else{
+            //     map.put(nums[i],1)
+            // }
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        
+        for(Integer key:map.keySet()){
+            if(map.get(key)>nums.length/2){
+               return key;
+            }
+        }
+        return -1;
+    }
+}
