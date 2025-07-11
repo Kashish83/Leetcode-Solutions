6b881ae9514e1a1331a11118298891a620fb1e34
kashish wadhwa <kashish1183.becse24@chitkara.edu.in>
2025-07-11 23:16:40 +0530

    Added 111. MinimumDepthOfBinaryTree.java

---
+++ 111. MinimumDepthOfBinaryTree.java
@@ -0,0 +1,26 @@
+import java.util.*;
+ class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) {
+        val = x;
+    }
+}
+class Solution {
+    public int minDepth(TreeNode root) {
+        if(root==null){
+            return 0;
+
+        }
+        if(root.left==null){
+            return 1 + minDepth(root.right);
+        }
+        if(root.right==null){
+            return 1 + minDepth(root.left);
+        }
+        int leftDepth=minDepth(root.left);
+        int rightDepth=minDepth(root.right);
+        return 1+Math.min(leftDepth,rightDepth);
+    }
+}
