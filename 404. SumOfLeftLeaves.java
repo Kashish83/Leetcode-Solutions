461c1a663734d2338ff5ec033af13edb1f9cae84
kashish wadhwa <kashish1183.becse24@chitkara.edu.in>
2025-07-25 22:49:44 +0530

    Added 404. SumOfLeftLeaves.java

---
+++ 404. SumOfLeftLeaves.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        int sum=0;
+        if(root.left!=null && root.left.left==null && root.left.right==null){
+            sum += root.left.val;
+        }
+        sum+= sumOfLeftLeaves(root.left);
+         sum+= sumOfLeftLeaves(root.right);
+        return sum;
+    }
+}
