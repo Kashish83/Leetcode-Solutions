e6d72db02e612bb69bc2746deba44eca3cf9613b
kashish wadhwa <kashish1183.becse24@chitkara.edu.in>
2025-07-31 14:29:02 +0530

    Added 763. PartitionLabels.java

---
+++ 763. PartitionLabels.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        int lastindex[]=new int[26];
+        for(int i=0; i<s.length(); i++){
+          lastindex[s.charAt(i)-'a']=i;
+        }
+        List<Integer> result=new ArrayList<>();
+        int start=0, end=0;
+        for(int i=0; i<s.length(); i++){
+            end=Math.max(end,lastindex[s.charAt(i)-'a']);
+            if(i==end){
+                result.add(end-start+1);
+                start=i+1;
+            }
+        }
+        return result;
+    }
+}
