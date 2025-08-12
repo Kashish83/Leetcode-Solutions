28652259207f81fed14c805d6f9d15c559e37a2d
kashish wadhwa <kashish1183.becse24@chitkara.edu.in>
2025-08-12 23:03:32 +0530

    Added 53. MaximumSubarray.java

---
+++ 53. MaximumSubarray.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max=nums[0];
+        int currSum=0;
+        for(int num:nums){
+            currSum+=num;
+            if(currSum>max){
+                max=currSum;
+            }
+            if(currSum<0){
+                currSum=0;
+            }
+        }
+        return max;
+    }
+}
