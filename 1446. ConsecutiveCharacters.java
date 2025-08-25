2546d78327d888b34dc0b676bb7157c91d98f901
kashish wadhwa <kashish1183.becse24@chitkara.edu.in>
2025-08-25 23:03:25 +0530

    Added 1446. ConsecutiveCharacters.java

---
+++ 1446. ConsecutiveCharacters.java
@@ -0,0 +1,20 @@
+
+class Solution {
+    public int maxPower(String s) {
+      if(s.length()==0){
+        return 0;
+      }
+      int maxCount=1;
+      int count=1;
+      for(int i=1; i<s.length();i++){
+      if(s.charAt(i)==s.charAt(i-1)){
+        count++;
+        maxCount=Math.max(maxCount,count);
+      }else{
+        count=1;
+      }
+      }
+
+      return maxCount;  
+    }
+}
