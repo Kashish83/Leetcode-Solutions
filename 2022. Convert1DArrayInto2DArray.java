45f96225d05fe99c5847eeacc5c739fabaa501c3
kashish wadhwa <kashish1183.becse24@chitkara.edu.in>
2025-08-30 21:58:31 +0530

    2022. Convert1DArrayInto2DArray.java

---
+++ 2022. Convert1DArrayInto2DArray.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[][] construct2DArray(int[] original, int m, int n) {
+        if(original.length!=m*n){
+            return new int[0][0];
+        }
+        int [][]arr2D= new int[m][n];
+        for(int i=0;i<original.length; i++){
+         arr2D[i/n][i%n]=original[i];
+        }
+        return arr2D;
+    }
+}
