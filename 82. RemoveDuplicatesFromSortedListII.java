df19cf3252383678b901497acf8bf79815d73a62
kashish wadhwa <kashish1183.becse24@chitkara.edu.in>
2025-11-13 23:44:59 +0530

    Added 82. RemoveDuplicatesFromSortedListII.java

---
+++ 82. RemoveDuplicatesFromSortedListII.java
@@ -0,0 +1,25 @@
+
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head==null || head.next==null){
+            return head;
+        }
+        ListNode dummy= new ListNode(0);
+        dummy.next=head;
+        ListNode prev=dummy;
+        ListNode current=head;
+        while(current!=null){
+            if(current.next!=null && current.val==current.next.val){
+                while(current.next!=null && current.val==current.next.val){
+                    current=current.next;
+                }
+                prev.next=current.next;
+            }else{
+                prev=prev.next;
+            }
+            current=current.next;
+        }
+        return dummy.next;
+    }
+
+}
